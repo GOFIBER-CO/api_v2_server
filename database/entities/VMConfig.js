4fdb806296f9c92089c83dc8083007f898814e2f
trannhon70 <kevintran351996@gmail.com>
2022-12-28 09:41:04 +0700

    model VM config

---
+++ database/entities/VMConfig.js
@@ -0,0 +1,166 @@
+require('../database');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+let VMConfigchema = new Schema({
+    ParentCheckpointId: {
+        type: String,
+    },
+    ParentCheckpointName: {
+        type: String,
+    },
+    VMName :{
+        type: String,
+    },
+    VMId :{
+        type: String,
+    },
+    CheckpointFileLocation :{
+        type: String,
+    },
+    ConfigurationLocation :{
+        type: String,
+    },
+    SmartPagingFileInUse :{
+        type: Boolean,
+    },
+    SmartPagingFilePath :{
+        type: String,
+    },
+    SnapshotFileLocation :{
+        type: String,
+    },
+    AutomaticStartAction :{
+        type: String,
+    },
+    AutomaticStartDelay :{
+        type: Number,
+    },
+    AutomaticStopAction :{
+        type: String,
+    },
+    AutomaticCriticalErrorAction :{
+        type: String,
+    },
+    AutomaticCriticalErrorActionTimeout :{
+        type: Number,
+    },
+    AutomaticCheckpointsEnabled :{
+        type: Boolean,
+    },
+    CPUUsage  :{
+        type: Number,
+    },
+    MemoryAssigned:{
+        type: Number,
+    },
+    MemoryDemand:{
+        type: String,
+    },
+    NumaAligned:{
+        type: String,
+    },
+    NumaNodesCount:{
+        type: Number,
+    },
+    NumaSocketCount:{
+        type: Number,
+    },
+    Heartbeat:{
+        type: String,
+    },
+    IntegrationServicesState :{
+        type: String,
+    },
+    IntegrationServicesVersion :{
+        type: String,
+    },
+    Uptime:{
+        type: String,
+    },
+    OperationalStatus:{
+        type: String,
+    },
+    PrimaryOperationalStatus:{
+        type: String,
+    },
+    SecondaryOperationalStatus:{
+        type: String,
+    },
+    StatusDescriptions:{
+        type: String,
+    },
+    PrimaryStatusDescription:{
+        type: String,
+    },
+    SecondaryStatusDescription:{
+        type: String,
+    },
+    Status:{
+        type: String,
+    },
+    ReplicationHealth:{
+        type: String,
+    },
+    ReplicationMode:{
+        type: String,
+    },
+    ReplicationState:{
+        type: String,
+    },
+    ResourceMeteringEnabled:{
+        type: Boolean,
+    },
+    CheckpointType:{
+        type: String,
+    },
+    EnhancedSessionTransportType:{
+        type: String,
+    },
+    Groups:{
+        type: String,
+    },
+    Version:{
+        type: String,
+    },
+    VirtualMachineType:{
+        type: String,
+    },
+    VirtualMachineSubType:{
+        type: String,
+    },
+    Notes :{
+        type: String,
+    },
+    State :{
+        type: String,
+    },
+    ComPort1 :{
+        type: String,
+    },
+    ComPort2 :{
+        type: String,
+    },
+    DVDDrives :{
+        type: String,
+    },
+    FibreChannelHostBusAdapters :{
+        type: String,
+    },
+    FloppyDrive :{
+        type: String,
+    },
+    
+    createdTime: {
+        type: Date,
+        default: Date.now
+    },
+    updatedTime: {
+        type: Date
+    },
+}, {versionKey: false});
+
+VMConfigchema.index({'VMName': 'text'});
+
+module.exports = mongoose.model('VMConfig', VMConfigchema)
