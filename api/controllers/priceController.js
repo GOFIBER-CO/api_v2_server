4ecab8d257e3b9fa886a69f4b2eaa88392d5a2ec
PhuongNguyn <zinzin.vung@gmail.com>
2023-02-09 13:42:43 +0700

    update

--- api/controllers/priceController.js
+++ api/controllers/priceController.js
@@ -2,7 +2,6 @@ const priceModel = require('../../database/entities/Price')
 class PriceController {
     async initPrice(){
         try {
-            
         } catch (error) {
             console.log(error)
             return res.status(500).json({message: 'Failed'})
